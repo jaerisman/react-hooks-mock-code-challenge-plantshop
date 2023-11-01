cbb53e18a69958ce2766027fc23c960a281791b9
Jacob Erisman <jacob.erisman@gmail.com>
2023-11-01 16:45:32 -0400

    lab complete

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
+import NewPlantForm from "./NewPlantForm";
+import Search from "./Search";
 
 function PlantList() {
+  const [plants, setPlants] = useState([]); 
+  const [searchQuery, setSearchQuery] = useState("");
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const addPlant = (newPlant) => {
+    setPlants((prevPlants) => [...prevPlants, newPlant])
+  };
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <div>
+      <Search handleSearch={handleSearch} />
+      <NewPlantForm addPlant={addPlant} />
+      <ul className="cards">
+        {filteredPlants.map((plant) => (
+          <PlantCard key={plant.id} plant={plant} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
